d2c1cf59805c7ad046be14825023762770c6703d
gaurav gupta <sibbugaurav2001@gmail.com>
2024-05-06 00:30:49 +0530

    first commit

--- app.py
+++ app.py
@@ -67,9 +67,8 @@ disease_model.eval()
 
 # Loading crop recommendation model
 
-crop_recommendation_model_path = 'models/RandomForest.pkl'
-crop_recommendation_model = pickle.load(
-    open(crop_recommendation_model_path, 'rb'))
+dtr=pickle.load(open('dtr.pkl','rb'),encoding="ASCII")
+preprocessor=pickle.load(open('preprocessor.pkl','rb'))
 
 
 # =========================================================================================
@@ -167,29 +166,52 @@ def fertilizer_recommendation():
 
 @ app.route('/crop-predict', methods=['POST'])
 def crop_prediction():
-    title = 'Harvestify - Crop Recommendation'
+    title = 'PrediCrop - Crop Recommendation'
+    if request.method=='POST':
+            State_Name=request.form['State_Name']
+            District_Name=request.form['District_Name']
+            Crop_Year=request.form['Crop_Year']
+            Season=request.form['Season']
+            Area=request.form['Area']
 
-    if request.method == 'POST':
-        N = int(request.form['nitrogen'])
-        P = int(request.form['phosphorous'])
-        K = int(request.form['pottasium'])
-        ph = float(request.form['ph'])
-        rainfall = float(request.form['rainfall'])
+            crops = ['Arecanut', 'Other Kharif pulses', 'Rice', 'Banana', 'Cashewnut',
+            'Coconut ', 'Dry ginger', 'Sugarcane', 'Sweet potato', 'Tapioca',
+            'Black pepper', 'Dry chillies', 'other oilseeds', 'Turmeric',
+            'Maize', 'Moong(Green Gram)', 'Urad', 'Arhar/Tur', 'Groundnut',
+            'Sunflower', 'Bajra', 'Castor seed', 'Cotton(lint)', 'Horse-gram',
+            'Jowar', 'Korra', 'Ragi', 'Tobacco', 'Gram', 'Wheat', 'Masoor',
+            'Sesamum', 'Linseed', 'Safflower', 'Onion', 'other misc. pulses',
+            'Samai', 'Small millets', 'Coriander', 'Potato',
+            'Other  Rabi pulses', 'Soyabean', 'Beans & Mutter(Vegetable)',
+            'Bhindi', 'Brinjal', 'Citrus Fruit', 'Cucumber', 'Grapes', 'Mango',
+            'Orange', 'other fibres', 'Other Fresh Fruits', 'Other Vegetables',
+            'Papaya', 'Pome Fruit', 'Tomato', 'Mesta', 'Cowpea(Lobia)',
+            'Lemon', 'Pome Granet', 'Sapota', 'Cabbage', 'Rapeseed &Mustard',
+            'Peas  (vegetable)', 'Niger seed', 'Bottle Gourd', 'Varagu',
+            'Garlic', 'Ginger', 'Oilseeds total', 'Pulses total', 'Jute',
+            'Peas & beans (Pulses)', 'Blackgram', 'Paddy', 'Pineapple',
+            'Barley', 'Sannhamp', 'Khesari', 'Guar seed', 'Moth',
+            'Other Cereals & Millets', 'Cond-spcs other', 'Turnip', 'Carrot',
+            'Redish', 'Arcanut (Processed)', 'Atcanut (Raw)',
+            'Cashewnut Processed', 'Cashewnut Raw', 'Cardamom', 'Rubber',
+            'Bitter Gourd', 'Drum Stick', 'Jack Fruit', 'Snak Guard', 'Tea',
+            'Coffee', 'Cauliflower', 'Other Citrus Fruit', 'Water Melon',
+            'Total foodgrain', 'Kapas', 'Colocosia', 'Lentil', 'Bean',
+            'Jobster', 'Perilla', 'Rajmash Kholar', 'Ricebean (nagadal)',
+            'Ash Gourd', 'Beet Root', 'Lab-Lab', 'Ribed Guard', 'Yam',
+            'Pump Kin', 'Apple', 'Peach', 'Pear', 'Plums', 'Litchi', 'Ber',
+            'Other Dry Fruit', 'Jute & mesta']
 
-        # state = request.form.get("stt")
-        city = request.form.get("city")
+            predicted_values = {}
+            for crop in crops:
+                features = np.array([[State_Name, District_Name, Crop_Year, Season, crop, Area]])
+                t_features = preprocessor.transform(features)
+                p_v=(dtr.predict(t_features).reshape(1, -1))
+                predicted_values[crop] = p_v.item()
 
-        if weather_fetch(city) != None:
-            temperature, humidity = weather_fetch(city)
-            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
-            my_prediction = crop_recommendation_model.predict(data)
-            final_prediction = my_prediction[0]
-
-            return render_template('crop-result.html', prediction=final_prediction, title=title)
-
-        else:
-
-            return render_template('try_again.html', title=title)
+            predicted_values = sorted(predicted_values.items(), key=lambda x: x[1], reverse=True)
+         
+            return render_template('crop-result.html', predicted_values=predicted_values)
 
 # render fertilizer recommendation result page
 
